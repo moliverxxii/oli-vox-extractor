bb4d3703a8dc1e6b489a831310168847fb04db68
Oliver Michalowski <moliver2204@gmail.com>
2023-04-23 17:49:13 +0200

    Réparation des normes

--- oli_norms.m
+++ oli_norms.m
@@ -1,24 +1,23 @@
-function norms = oli_norms(scan,n_extr)
-n_scan = length(scan);
-a_scan  = scan.^2;
-filt_b = ones(n_extr,1);
-n_norms = n_scan + n_extr -1;
+function nm_output = oli_norms(au_input,n_block)
+[n_input, c_input] = size(au_input);
+au_input_sq  = au_input.^2;
+filt_b = ones(n_block,1);
+n_norms = n_input + n_block - 1;
 n_fft = 2^ceil(log2(n_norms));
 
-norms_f = fft(a_scan, n_fft);
-norms_f = norms_f .* fft(filt_b, n_fft);
+fr_nm_output = fft(au_input_sq, n_fft);
+fr_nm_output = fr_nm_output .* fft(filt_b, n_fft);
 
-norms = zeros(n_fft,1);
-norms = real(ifft(norms_f));
-
-norms = norms(1:n_norms);
-norms = abs(norms.^.5);
-for i = (3:n_norms)
-    if(norms(i-1)==0)
-	norms(i-1) = .5*(norms(i-2)+norms(i));
+nm_output = zeros(n_fft,c_input);
+nm_output = ifft(fr_nm_output);
+nm_output = nm_output(1:n_norms,:);
+nm_output = abs(nm_output).^.5;
+oli_norms_param = false;
+if(oli_norms_param)
+    for i = (3:n_norms)
+	if(nm_output(i-1)==0)
+	    nm_output(i-1) = .5*(nm_output(i-2)+nm_output(i));
+	end
     end
-
-
 end
-plot(norms)
 end
--- oli_norms_mp.m
+++ oli_norms_mp.m
@@ -7,10 +7,17 @@ function [nm_norms_mp, p_norms] = oli_norms_mp(au_input, n_block)
 p_input_block = (-n_input+1):(n_block-1);
 
 nm_norms_mp = zeros(length(p_input_block),c_input);
-for k_norms = 1:length(p_input_block)
-    rg_norms = max(1,             1 - p_input_block(k_norms)):...
-               min(n_input, n_block - p_input_block(k_norms));
-    nm_norms_mp(k_norms,:) =  sum(au_input(rg_norms,:).^2);
+naive = false;
+if(naive)
+    n_norms_mp = length(p_input_block);
+    for k_norms = 1:n_norms_mp
+	rg_norms = max(1,             1 - p_input_block(k_norms)):...
+		   min(n_input, n_block - p_input_block(k_norms));
+	disp(rg_norms([1,end]))
+	nm_norms_mp(k_norms,:) =  sum(au_input(rg_norms,:).^2,1)
+    end
+    nm_norms_mp = nm_norms_mp.^.5;
+else
+    nm_norms_mp = oli_norms(au_input, n_block)(end:-1:1,:);
 end
-nm_norms_mp = nm_norms_mp.^.5;
 end
