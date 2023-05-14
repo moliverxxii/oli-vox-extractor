a9c9ce249f6465adb81b3d238e8fa1d571ea0ee8
Oliver Michalowski <moliver2204@gmail.com>
2023-05-14 09:25:49 +0200

    Simplification de l'étape de synthèse du bloc vocal. Ajout d'une étape de fenêtrage.

--- oli_cancel_parameters.m
+++ oli_cancel_parameters.m
@@ -12,19 +12,17 @@ nm_block                = sum(abs(au_x(rg_block_x,:)).^2,1).^.5;
 [cv_block_y, p_block_y] = oli_cov(au_x(rg_block_x,:),au_y);
 cr_block_y              = cv_block_y ./(nm_y_mp .* nm_block);
 
-
 [cr1_max_block_y, n1_max_block_y] =...
         max(abs(cr_block_y),[],1);
 [cr2_block_instru_max, n2_max_block_y] =...
         max(cr1_max_block_y,[],2);
 n3_max_block_y = n1_max_block_y(n2_max_block_y);
 %Le décalage à appliquer.
-p_max_x_y = p_block_y(n3_max_block_y);
+p_max_x_y = p_block_y(n3_max_block_y) + n_x_start - 1;
 %Le coefficient de projection.
 r_max_x_y = cv_block_y(n3_max_block_y,:)./(nm_y_mp(n3_max_block_y,:).^2);
 %Le coefficient de corrélation.
 cr_max_x_y = cr_block_y(n3_max_block_y);
-
-plot(p_block_y, cr_block_y)
+%plot(p_block_y, cr_block_y)
 
 end
--- vocaux.m
+++ vocaux.m
@@ -16,33 +16,37 @@
 
 %Paramètres
 %Reconstruction
-t_block = 2   %Durée d'un bloc de reconstruction. (secondes)
+t_block = 1.06 %Durée d'un bloc de reconstruction. (secondes)
 %r_block      %Taux de recouvrement (pourcents).
 
 %Recherche de la position globale.
-c_block2 = 5; % t_block2/t_block: Nombre de blocs de reconstruction (block)
+c_block2 = 12; % t_block2/t_block: Nombre de blocs de reconstruction (block)
                  % dans un bloc de recherche de positions (block2).
 t_block2 = c_block2 * t_block % Durée d'un bloc de recherche de position.
-t_tail   = t_block/4          % Durée de la queue avant et après un bloc de
                               % recherge de position pour réglage fin.
-c_block3 = 400;
-t_block3 = t_block/c_block3;
+t_block3 = .023;
+c_block3 = floor(t_block/t_block3);
+t_block3 = t_block/c_block3
+c_block3 = int32(c_block3);
+
+t_tail   = t_block/2          % Durée de la queue avant et après un bloc de
 
 "Vocaux"
 "Récupération des sources"
 [au_instru, au_full, f_samplerate] = oli_load_tracks(tx_title);
 
 "Initialisation"
-n_block  = floor(f_samplerate * t_block);
+n_block  = int32(floor(f_samplerate * t_block));
 n_block2 = c_block2 * n_block;
 n_block3 = n_block/c_block3
-n_tail   = floor(f_samplerate * t_tail);
+n_hop    = floor(n_block/2);
+n_tail   = int32(floor(f_samplerate * t_tail));
 
 [n_instru, c_instru] = size(au_instru);
 [n_full,   c_full]   = size(au_full);
 
 %Initialisation du flux audio
-au_vox = zeros(n_full,c_full);
+au_vox = zeros(n_full, c_full);
 
 %"Calcul des normes"
 %norms_instru_mp = oli_norms_mp(au_instru(:,1), n_block);
@@ -51,71 +55,64 @@ au_vox = zeros(n_full,c_full);
 [nm_full_block,   rg_full_block]   = oli_block_norm(au_full,   n_block3);
 [nm_instru_block, rg_instru_block] = oli_block_norm(au_instru, n_block3);
 
-tm_init      = time;
 b_last_block = false;
 %Calcul d'un bloc de recherche de positions (block2).
 for k_block2 = 0:n_block2:(n_full-1)
     %On trouve le meilleur extrait a soustraire.
     if(k_block2 + n_block2 > n_full)
         disp("Dernier bloc.")
-	n_block2 = n_full - k_block2;
-
+        n_block2 = n_full - k_block2;
     end
+
     au_full(k_block2+[1,n_block2],:);
-    n_block32 = floor(n_block2/n_block3);
-    rg_nm_full = k_block2/n_block3  + [1: n_block32]; %TODO FLOOR OU CEIL???
-    %TODO ICI trouver la valeur n_x_start
+    n_block32 = floor(n_block2/n_block3); 
+    rg_nm_full = k_block2/n_block3  + (1: n_block32); %TODO FLOOR OU CEIL???
     p_max_nm_block2_instru = oli_cancel_parameters(nm_full_block,...
                                   nm_instru_block,...
-				  rg_nm_full(1),...
-				  n_block32);
+                                  rg_nm_full(1),...
+                                  n_block32);
     disp(-p_max_nm_block2_instru*t_block3);
+    p2 = p_max_nm_block2_instru * n_block2/n_block3;
 
-				  
-
-    %au_instru2 = au_instru(rg_instru2,:);
 
     %Calcul d'un bloc de reconstruction (block).
-    for k_block = k_block2 + n_block*(0:c_block2 - 1)
+    for k_block = k_block2 + (0:n_hop:(n_block*(c_block2)-1))
         %Calcul de la covacovariance
         %p_corr: (-ny+1 : n_x-1)
         % k = p_corr + N_y - 1
-	if(k_block + n_block > n_full)
-	    disp("Dernier bloc.")
-	    b_last_block = true;
-	    n_block = n_full - k_block;
-	end
-	rg_block = k_block + (1:n_block);
+        if(k_block + n_block > n_full)
+            disp("Dernier bloc.")
+            b_last_block = true;
+            n_block = n_full - k_block;
+        end
+        rg_block = k_block + (1:n_block);
+        au_block = au_full(rg_block, :);
+        rg_instru2_low = ...
+           1 + max(0,k_block - n_tail - p2);
+        n_instru2 = (n_block + 2*n_tail);
+        rg_instru2 = ...
+           (0:(n_instru2-1)) + min(rg_instru2_low, n_instru-n_instru2 + 1);
 
+        au_instru2 = au_instru(rg_instru2,:);
 
-        %Calcul de la norme de l'extrait
-        %nm_block = sum(abs(au_block).^2,1).^.5;
-
-        %Calcul de la correlation
-
-        %On trouve la position dans l'extrait
-
-        %Ajout aux statistiques, delta temporel (échantillon et
-        %secondes), corrélation.
-
-        %Calcul du range
-        %n_max_corr_start = rg_instru2(1) - p_block_instru2_max;
-        %n_max_corr_end   = n_max_corr_start + c_block3*n_block - 1;
-
-        %rg_max_corr = max(1,n_max_corr_start):min(n_instru,n_max_corr_end);
-
-
-        %Calcul du coefficient.
-	
         %Ajout du bloc au flux.
-        %au_vox(rg_block, :) = oli_subtract_block(au_full, au_instru,n_full_start,  n_block, p_max_corr, coefficient); %TODO: DANGER
-	if(b_last_block)
-	    break;
-	end
+        r_window = ones(n_block,1);
+        if(!b_last_block)
+            if(0 != k_block)
+                r_window(1:n_hop) = (1:n_hop)/n_hop;
+            end
+            r_window(n_hop+1:n_block) = (n_block-n_hop:-1:1)/n_hop;
+        else
+            r_window(1:n_block) = (1:n_block)/n_hop;
+        end
+        au_vox(rg_block, :) += r_window .* oli_subtract_block(au_block, au_instru2);
+        if(b_last_block)
+            break;
+        end
     end
 end
 %Écriture du fichier.
-%oli_save_track(au_vox, f_samplerate, tx_title);
+oli_save_track(au_vox, f_samplerate, tx_title);
 %Écriture du ficher statistique.
 disp("Fin")
 
